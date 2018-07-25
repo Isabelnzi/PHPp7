972145f415171cf5224a7a2256282f9f0b477478
Nzingani <isabelnzingani@outlook.com>
2018-07-25 21:41:50 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8" />
+    <title>exo7</title>
+  </head>
+  <body>
+    <p>
+      <?php
+      $lastname = 'nzingani';
+      $firstname = 'isabel';
+      $age = 35;
+        echo 'Bonjour '.$lastname.' '.$firstname.', tu as '.$age.' ans?';
+      ?>
+    </p>
+  </body>
